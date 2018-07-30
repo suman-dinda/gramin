f9e83f0ac26b25d903553159d4786b9f564370b7
Suman HP <sumandinda123@gmail.com>
2018-07-30 08:19:19 +0530

    stock_transfer conpleted

--- api/assignStock.php
+++ api/assignStock.php
@@ -14,14 +14,15 @@ $status = 1;
 
 $sql = "INSERT INTO stock_list (`product_id`,`product_name`,`user_unique`,`user_id`,`stock_requested`,`stock_unit`,`req_id`,`status`) VALUES('$prd_id','$prd_name','$usr_unique','$user_id','$stock_requested','$stock_assigned','$req_id',1)";
 $result = $conn->execute($sql);
-print($result);
-// if($result == 1){
-// 	$sql2 = "SELECT product_unit FROM products WHERE id='$prd_id'";
-// 	$res = $conn->getOne($sql2);
-// 	if(isset($res)){
-// 		$de
-// 	}
-// }
+if($result == 1){
+	$sql2 = "UPDATE `requestedstock` SET `status`=2 WHERE `id`='$req_id'";
+	$res = $conn->execute($sql2);
+	
+	$updateActualStock = "UPDATE `products` SET `product_unit`=(product_unit - $stock_assigned) WHERE `id` = '$prd_id'";
+	$rez = $conn -> execute($updateActualStock);
+
+	print($result."-".$res."-".$rez);
+}
 
 
 ?>

f51a3eb47e3ecad6a7f4c3979269a7a298bb06f5
sumandinda007 <sumandinda123@gmail.com>
2018-08-03 20:28:27 +0530

    sale management

--- assets/js/services/saleManagement.service.js
+++ assets/js/services/saleManagement.service.js
@@ -12,4 +12,19 @@ app.service("saleManagement", function($http,$log){
             throw response;
 	    });
 	}
+
+	this.getSaleDataForUser = (userkey) =>{
+		var data = {userKey : userkey};
+		return $http({
+	        method : "POST",
+	        url : "../api/user/getUserSaleList.php",
+	        params: data,
+	        headers: { 'Content-Type': 'application/json; charset=UTF-8' }
+	    }).then(function success(response) {
+	        return response.data;
+	    }, function error(response) {
+	        $log.error('ERROR:', response);
+            throw response;
+	    });
+	}
 });

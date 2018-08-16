25991090a4a341730a26cf960a58fe7f3fb05d22
sumandinda007 <sumandinda123@gmail.com>
2018-08-16 16:54:22 +0530

    create product php
    add file upload system

--- api/createProduct.php
+++ api/createProduct.php
@@ -2,20 +2,51 @@
 include_once '../server/connection.php';
 $conn = new Connection();
 
-$prdCode = $_REQUEST['prd_code'];;
-$prdName = $_REQUEST['prd_name'];
-$prdUnit = $_REQUEST['prd_unit'];
-$prdSize = $_REQUEST['prd_size'];
-$prdCategory = $_REQUEST['prd_category'];
-$prdSubCategory = $_REQUEST['prd_subcategory'];
-$prdBrand = $_REQUEST['prd_brand'];
-$prdCost = $_REQUEST['prd_cost'];
-$prdDealerPrice = $_REQUEST['prd_delear_price'];
-$prdTax = $_REQUEST['prd_tax'];
-$prdDescription = $_REQUEST['prd_description'];
-$status = 1;
+// Location
+$location = '../upload/';
 
-$sql = "INSERT INTO `products` (`product_code`,`product_name`,`product_unit`,`product_size`,`product_category`,`product_subcategory`,`product_brand`,`product_cost`,`product_dealerprice`,`product_tax`,`product_description`,`status`) VALUES('$prdCode','$prdName','$prdUnit','$prdSize','$prdCategory','$prdSubCategory','$prdBrand','$prdCost','$prdDealerPrice','$prdTax','$prdDescription','$status')";
-$result = $conn->execute($sql);
-print($result);
+if(isset($_FILES)){
+	// Count total files
+	$countfiles = count($_FILES['file']['name']);
+
+	$filename_arr = array(); 
+	// Looping all files
+
+	for ( $i = 0;$i < $countfiles;$i++ ){
+
+	   	//$filename = $_FILES['file']['name'][$i];
+	   	$temp = explode(".", $_FILES["file"]["name"][$i]);
+	   	$filename = round(microtime(true)) . '.' . end($temp);
+	   	// Upload file
+	   	move_uploaded_file($_FILES['file']['tmp_name'][$i],$location.$filename);
+	    
+	  	$filename_arr[] = $filename;
+
+	}
+
+	$arr = array('name' => $filename_arr);
+	$imageList = implode(",", $arr['name']);
+
+	$prdCode = $_REQUEST['prd_code'];;
+	$prdName = $_REQUEST['prd_name'];
+	$prdUnit = $_REQUEST['prd_unit'];
+	$prdSize = $_REQUEST['prd_size'];
+	$prdCategory = $_REQUEST['prd_category'];
+	$prdSubCategory = $_REQUEST['prd_subcategory'];
+	$prdBrand = $_REQUEST['prd_brand'];
+	$prdCost = $_REQUEST['prd_cost'];
+	$prdDealerPrice = $_REQUEST['prd_delear_price'];
+	$prdTax = $_REQUEST['prd_tax'];
+	$prdDescription = $_REQUEST['prd_description'];
+	$status = 1;
+
+	$sql = "INSERT INTO `products` (`product_code`,`product_name`,`product_unit`,`product_size`,`product_category`,`product_subcategory`,`product_brand`,`product_cost`,`product_dealerprice`,`product_tax`,`product_description`,`product_images`,`status`) VALUES('$prdCode','$prdName','$prdUnit','$prdSize','$prdCategory','$prdSubCategory','$prdBrand','$prdCost','$prdDealerPrice','$prdTax','$prdDescription','$imageList','$status')";
+	$result = $conn->execute($sql);
+	print($result);
+}
+
+
+//echo json_encode($arr);
+//print_r($_REQUEST);
+
 ?>

3d9d28232791cfa4e8f8ce2ba28cb47ca9fa3d2d
sumandinda007 <sumandinda123@gmail.com>
2018-08-16 16:56:51 +0530

    new sql file

--- gramin.sql
+++ gramin.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Aug 13, 2018 at 04:02 AM
--- Server version: 5.7.19
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: Aug 16, 2018 at 01:26 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 5.6.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,9 +28,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `products`
 --
 
-DROP TABLE IF EXISTS `products`;
-CREATE TABLE IF NOT EXISTS `products` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `products` (
+  `id` int(10) NOT NULL,
   `product_code` varchar(20) NOT NULL,
   `product_name` varchar(30) NOT NULL,
   `product_unit` varchar(20) NOT NULL,
@@ -42,19 +41,20 @@ CREATE TABLE IF NOT EXISTS `products` (
   `product_dealerprice` varchar(20) NOT NULL,
   `product_tax` varchar(10) NOT NULL,
   `product_description` text NOT NULL,
-  `status` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `product_images` text,
+  `status` int(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_unit`, `product_size`, `product_category`, `product_subcategory`, `product_brand`, `product_cost`, `product_dealerprice`, `product_tax`, `product_description`, `status`) VALUES
-(1, 'SKU-1531328997147', 'productName', '9900', '3x4x5', 'Shirts', 'Checked Shirts', 'brand.brand_name', '12000', '11000', '5', 'product description', 1),
-(2, 'SKU-1531329234582', 'productName1', '40', '3x4x7', 'Shirts', 'Checked Shirts', 'brand.brand_name', '1200', '1000', '5', 'product description details', 1),
-(3, 'SKU-1531331120512', 'productName3', '100', '3x4x7', 'Shirts', 'Checked Shirts', 'brand.brand_name', '12000', '12000', '5', 'product descriptions 90', 1),
-(4, 'SKU-1531331249225', 'productName3', '100', '3x4x7', 'Shirts', 'Checked Shirts', 'brand.brand_name', '12000', '11000', '5', 'product description', 1);
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_unit`, `product_size`, `product_category`, `product_subcategory`, `product_brand`, `product_cost`, `product_dealerprice`, `product_tax`, `product_description`, `product_images`, `status`) VALUES
+(1, 'SKU-1531328997147', 'productName', '9900', '3x4x5', 'Shirts', 'Checked Shirts', 'brand.brand_name', '12000', '11000', '5', 'product description', NULL, 1),
+(2, 'SKU-1531329234582', 'productName1', '40', '3x4x7', 'Shirts', 'Checked Shirts', 'brand.brand_name', '1200', '1000', '5', 'product description details', NULL, 1),
+(3, 'SKU-1531331120512', 'productName3', '100', '3x4x7', 'Shirts', 'Checked Shirts', 'brand.brand_name', '12000', '12000', '5', 'product descriptions 90', NULL, 1),
+(4, 'SKU-1531331249225', 'productName3', '100', '3x4x7', 'Shirts', 'Checked Shirts', 'brand.brand_name', '12000', '11000', '5', 'product description', NULL, 1),
+(5, 'SKU-1534416499849', 'productName', '100', '3x4x7', '', 'Checked Shirts', 'brand.brand_name', '12000', '10000', '12%', 'ahgdhwjgdhwgdgwajdgwgdjwa', '1534418528.png,1534418528.jpg,1534418528.jpg,1534418528.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -62,14 +62,12 @@ INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_unit`, `p
 -- Table structure for table `product_brand`
 --
 
-DROP TABLE IF EXISTS `product_brand`;
-CREATE TABLE IF NOT EXISTS `product_brand` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product_brand` (
+  `id` int(10) NOT NULL,
   `brand_name` varchar(30) NOT NULL,
   `brand_description` text,
-  `status` int(2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `status` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product_brand`
@@ -84,14 +82,12 @@ INSERT INTO `product_brand` (`id`, `brand_name`, `brand_description`, `status`) 
 -- Table structure for table `product_category`
 --
 
-DROP TABLE IF EXISTS `product_category`;
-CREATE TABLE IF NOT EXISTS `product_category` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product_category` (
+  `id` int(10) NOT NULL,
   `category_name` varchar(20) NOT NULL,
   `category_description` text,
-  `status` int(2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `status` int(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product_category`
@@ -107,16 +103,14 @@ INSERT INTO `product_category` (`id`, `category_name`, `category_description`, `
 -- Table structure for table `product_subcategory`
 --
 
-DROP TABLE IF EXISTS `product_subcategory`;
-CREATE TABLE IF NOT EXISTS `product_subcategory` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `product_subcategory` (
+  `id` int(10) NOT NULL,
   `subcategory_name` varchar(50) NOT NULL,
   `subcategory_desc` text,
   `category` varchar(20) NOT NULL,
   `category_name` varchar(20) DEFAULT NULL,
-  `status` int(2) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `status` int(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `product_subcategory`
@@ -132,17 +126,15 @@ INSERT INTO `product_subcategory` (`id`, `subcategory_name`, `subcategory_desc`,
 -- Table structure for table `requestedstock`
 --
 
-DROP TABLE IF EXISTS `requestedstock`;
-CREATE TABLE IF NOT EXISTS `requestedstock` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `requestedstock` (
+  `id` int(10) NOT NULL,
   `user_unique` varchar(30) NOT NULL,
   `user_id` int(10) NOT NULL,
   `product_id` int(10) NOT NULL,
   `product_name` varchar(50) NOT NULL,
   `stock_unit` int(10) NOT NULL,
-  `status` int(2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `status` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `requestedstock`
@@ -158,9 +150,8 @@ INSERT INTO `requestedstock` (`id`, `user_unique`, `user_id`, `product_id`, `pro
 -- Table structure for table `sales`
 --
 
-DROP TABLE IF EXISTS `sales`;
-CREATE TABLE IF NOT EXISTS `sales` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sales` (
+  `id` int(10) NOT NULL,
   `sale_no` varchar(20) NOT NULL,
   `sale_chalan` varchar(20) NOT NULL,
   `sale_date` varchar(20) NOT NULL,
@@ -178,9 +169,8 @@ CREATE TABLE IF NOT EXISTS `sales` (
   `sale_totbill` varchar(15) NOT NULL,
   `sale_note` text NOT NULL,
   `sale_userunique` varchar(10) NOT NULL,
-  `sale_paymode` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+  `sale_paymode` varchar(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `sales`
@@ -197,10 +187,9 @@ INSERT INTO `sales` (`id`, `sale_no`, `sale_chalan`, `sale_date`, `sale_customer
 -- Table structure for table `service_request`
 --
 
-DROP TABLE IF EXISTS `service_request`;
-CREATE TABLE IF NOT EXISTS `service_request` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `service_no` varchar(10) DEFAULT NULL,
+CREATE TABLE `service_request` (
+  `id` int(11) NOT NULL,
+  `service_no` int(10) DEFAULT NULL,
   `service_name` varchar(20) NOT NULL,
   `service_amount` int(10) NOT NULL,
   `service_date` varchar(15) NOT NULL,
@@ -211,16 +200,18 @@ CREATE TABLE IF NOT EXISTS `service_request` (
   `amount_paid` int(10) DEFAULT NULL,
   `amount_due` int(10) DEFAULT NULL,
   `userkey` varchar(10) DEFAULT NULL,
-  `status` int(5) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  `status` int(5) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `service_request`
 --
 
 INSERT INTO `service_request` (`id`, `service_no`, `service_name`, `service_amount`, `service_date`, `customer_name`, `customer_mobile`, `customer_address`, `payment_mode`, `amount_paid`, `amount_due`, `userkey`, `status`) VALUES
-(7, 'SR-6953', 'Pan Card', 100, '11/08/2018', 'Suman Dinda', '08050986742', 'SIruseri', 'cash', 50, 50, 'hSDAeop', 2);
+(4, NULL, 'Pan Card', 100, '07/08/2018', 'Suman Dinda', '08050986742', 'Near Miami SuperMarket, Munekolala\nMarathahalli', 'cash', NULL, NULL, 'hSDAeop', 1),
+(3, NULL, 'Pan Card', 100, '05/08/2018', 'Suman Dinda', '08050986742', 'SIruseri', 'cash', NULL, NULL, 'hSDAeop', 1),
+(5, NULL, 'Pan Card', 100, '07/08/2018', 'Suman Dinda', '08050986742', 'Near Miami SuperMarket, Munekolala\nMarathahalli', 'cash', NULL, NULL, 'hSDAeop', 1),
+(6, NULL, 'Pan Card', 100, '07/08/2018', 'Suman D', '08050986742', 'sipcot it park chennai', 'cash', NULL, NULL, 'hSDAeop', 1);
 
 -- --------------------------------------------------------
 
@@ -228,13 +219,11 @@ INSERT INTO `service_request` (`id`, `service_no`, `service_name`, `service_amou
 -- Table structure for table `service_types`
 --
 
-DROP TABLE IF EXISTS `service_types`;
-CREATE TABLE IF NOT EXISTS `service_types` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `service_types` (
+  `id` int(10) NOT NULL,
   `service_name` varchar(20) NOT NULL,
-  `service_price` varchar(20) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `service_price` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `service_types`
@@ -249,9 +238,8 @@ INSERT INTO `service_types` (`id`, `service_name`, `service_price`) VALUES
 -- Table structure for table `stock_list`
 --
 
-DROP TABLE IF EXISTS `stock_list`;
-CREATE TABLE IF NOT EXISTS `stock_list` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `stock_list` (
+  `id` int(10) NOT NULL,
   `product_id` int(10) NOT NULL,
   `product_name` varchar(30) NOT NULL,
   `user_unique` varchar(20) NOT NULL,
@@ -259,9 +247,8 @@ CREATE TABLE IF NOT EXISTS `stock_list` (
   `stock_requested` int(10) NOT NULL,
   `stock_unit` int(20) NOT NULL,
   `req_id` int(10) NOT NULL,
-  `status` int(3) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `status` int(3) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `stock_list`
@@ -277,14 +264,12 @@ INSERT INTO `stock_list` (`id`, `product_id`, `product_name`, `user_unique`, `us
 -- Table structure for table `superadmin`
 --
 
-DROP TABLE IF EXISTS `superadmin`;
-CREATE TABLE IF NOT EXISTS `superadmin` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `superadmin` (
+  `id` int(10) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `token` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  `token` varchar(10) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `superadmin`
@@ -299,9 +284,8 @@ INSERT INTO `superadmin` (`id`, `username`, `password`, `token`) VALUES
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `u_id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `u_id` int(10) NOT NULL,
   `u_email` varchar(50) NOT NULL,
   `u_password` varchar(15) NOT NULL,
   `u_mobile` varchar(15) NOT NULL,
@@ -325,9 +309,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `u_assignedto` varchar(20) DEFAULT NULL,
   `u_usercreationdate` datetime DEFAULT NULL,
   `u_userdatachanged` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `u_userstatus` int(3) NOT NULL,
-  PRIMARY KEY (`u_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  `u_userstatus` int(3) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
@@ -340,6 +323,146 @@ INSERT INTO `users` (`u_id`, `u_email`, `u_password`, `u_mobile`, `u_unique`, `u
 (4, 'foodoku.in@gmail.com', 'racsw02s', '8050986742', 'FwSE0P0', 'grampanchayat', '4', 'ttyrytetettyey', '07/16/2013', 'canpd497r', '123456789012', 'district', 'Chennai', 'Newgen Softaware', 603103, '123456786', '23456', 'current', 'icici', '', 'gram_panchayat', NULL, '2018-06-29 22:04:50', '2018-06-29 22:04:50', 1),
 (5, 'sumandinda123@gmail.com', 'taluk', '8050986742', 'zIlL7Vs', 'talukdar', '1', 'fathersname', '07/15/1999', 'vadsdfsfd45465', '123456789012', 'district', 'Chennai', 'Newgen Softaware', 603103, '12rwerwerwerwer', 'ifsccode', 'savings', 'bank name', '1,2', 'taluk_head', 'HcNJNav', '2018-06-30 05:20:00', '2018-06-30 05:20:00', 1),
 (6, 'sumandinda123@gmail.com', 'z2ms02pl', '8050986742', 'HcNJNav', 'districthead', '1', 'fathersname', '07/25/2018', 'canpf454564e', '', 'district', 'Chennai', 'SIruseri', 603103, '1234567890-', 'sbin002123', 'savings', 'axis bank', '5', 'district_head', NULL, '2018-07-21 13:32:14', '2018-07-21 13:32:14', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_brand`
+--
+ALTER TABLE `product_brand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_category`
+--
+ALTER TABLE `product_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_subcategory`
+--
+ALTER TABLE `product_subcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `requestedstock`
+--
+ALTER TABLE `requestedstock`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `service_request`
+--
+ALTER TABLE `service_request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `service_types`
+--
+ALTER TABLE `service_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `stock_list`
+--
+ALTER TABLE `stock_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `product_brand`
+--
+ALTER TABLE `product_brand`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `product_category`
+--
+ALTER TABLE `product_category`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `product_subcategory`
+--
+ALTER TABLE `product_subcategory`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `requestedstock`
+--
+ALTER TABLE `requestedstock`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `service_request`
+--
+ALTER TABLE `service_request`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `service_types`
+--
+ALTER TABLE `service_types`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `stock_list`
+--
+ALTER TABLE `stock_list`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `u_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
